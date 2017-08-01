bf15fb7244280b138b078c7731f27e577a635a47
HiggstonRainbird <fberg.david@gmail.com>
2017-08-01 13:05:10 -0400

    2048 emulator working, but slow.

--- evolve-2048.py
+++ evolve-2048.py
@@ -28,13 +28,15 @@ and scaled like this:
     1/7     1/7   2/7     4/7
     3/7     5/7   1       5/7
 
+oldBoard = [[0,0,1./7,1./7],[0,0,1./7,2./7],[1./7,1./7,2./7,4./7],[3./7,5./7,7./7,5./7]]
+
 Upon a move, the program goes through each row or column:
     First, the row is sorted, with all non-zero elements considered identical.
     Second, each pair of identical elements are combined, the empty space replaced with a zero, and the score incremented.
     Third, the first step is repeated once (and only once).
 After each row or column has been scanned, the high score is increased (if necessary).
-Finally, a 2 or 4 is spawned randomly
-
+Finally, a 2 or 4 is spawned randomly in one of the positions currently occupied by 0.
+    A 4 has a 10% chance of spawning, while a 2 has a 90% chance.
 The fitness function is the score of the game, which is incremented by the value of any newly created block.
 
 This would likely be a LOT faster using numpy arrays rather than python lists of lists.
@@ -47,82 +49,96 @@ import visualize
 
 import math
 import random
+import copy
+import time
 
 
 # Note: This doesn't actually work yet.  I haven't finished debugging it.
-def next_move(board, highValue, score, move):
-    if move == 1:
+def next_move(oldBoard, highValue, score, move):
+    board = copy.deepcopy(oldBoard)
+    if move == 0:
         for row in board:
             row.sort(key = lambda x: 0 if x==0 else 1)
             for i in range(3):
-                if row[i] == row[i+1]:
-                    row[i] += 1. / math.log(2,highValue)
+                if row[i] == row[i+1] and row[i] != 0:
+                    row[i] += (1. / math.log(highValue,2))
                     row[i+1] = 0
-                    score += round(2**(row[i]*math.log(2,highValue)))
+                    score += round(2**(1.+row[i]*math.log(highValue,2)))
             row.sort(key = lambda x: 0 if x==0 else 1)
+    elif move == 1:
+        board = map(list, zip(*board))
+        for row in board:
+            row.sort(key = lambda x: 0 if x==0 else 1)
+            for i in range(3):
+                if row[i] == row[i+1] and row[i] != 0:
+                    row[i] += (1. / math.log(highValue,2))
+                    row[i+1] = 0
+                    score += round(2**(1.+row[i]*math.log(highValue,2)))
+            row.sort(key = lambda x: 0 if x==0 else 1)
+        board = map(list, zip(*board))
     elif move == 2:
-        board = map(list, zip(*board))
         for row in board:
-            row.sort(key = lambda x: 0 if x==0 else 1)
-            for i in range(3):
-                if row[i] == row[i+1]:
-                    row[i] += 1. / math.log(2,highValue)
-                    row[i+1] = 0
-                    score += round(2**(row[i]*math.log(2,highValue)))
-            row.sort(key = lambda x: 0 if x==0 else 1)
-        board = map(list, zip(*board))
+            row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
+            for i in range(3,0,-1):
+                if row[i] == row[i-1] and row[i] != 0:
+                    row[i-1] += (1. / math.log(highValue,2))
+                    row[i] = 0
+                    score += round(2**(1.+row[i]*math.log(highValue,2)))
+            row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
     elif move == 3:
-        for row in board:
-            row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
-            for i in range(4,1,-1):
-                if row[i] == row[i-1]:
-                    row[i-1] += 1. / math.log(2,highValue)
-                    row[i] = 0
-                    score += round(2**(row[i-1]*math.log(2,highValue)))
-            row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
-    elif move == 4:
         board = map(list, zip(*board))
         for row in board:
             row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
-            for i in range(4,1,-1):
-                if row[i] == row[i-1]:
-                    row[i-1] += 1. / math.log(2,highValue)
+            for i in range(3,0,-1):
+                if row[i] == row[i-1] and row[i] != 0:
+                    row[i-1] += (1. / math.log(highValue,2))
                     row[i] = 0
-                    score += round(2**(row[i-1]*math.log(2,highValue)))
+                    score += round(2**(1.+row[i]*math.log(highValue,2)))
             row.sort(key = lambda x: 0 if x==0 else 1, reverse=True)
         board = map(list, zip(*board))
     if max([max(row) for row in board]) > 1:
-        board = [[elem*(math.log(2,highValue)/(math.log(2,highValue) + 1)) for elem in row] for row in board]
+        board = [[elem*(math.log(highValue,2)/(math.log(highValue,2) + 1)) for elem in row] for row in board]
         highValue = 2 * highValue
     zeroPositions = []
     for i in range(4):
         for j in range(4):
             if board[i][j] == 0:
                 zeroPositions.append([i,j])
-    zeroPositions = random.choice(zeroPositions)
-    # In 2048, there is a 90% chance of a new tile being 2, and 10% chance of being 4.
-    board[zeroPositions[0]][zeroPositions[1]] = (2 if random.random() < 0.9 else 4)
+    try:
+        zeroPositions = random.choice(zeroPositions)
+        board[zeroPositions[0]][zeroPositions[1]] = ((1. / math.log(highValue,2)) if random.random() < 0.9 else (2. / math.log(highValue,2)))
+    except IndexError:
+        pass
     return board, highValue, score
 
-next_move(oldBoard, 128, 1000, 1)
-
-tictactoe_inputs = [tuple([random.choice([-1,0,1]) for i in range(9)]) for j in range(25)]
-tictactoe_outputs = [[int(i==0) for i in board] for board in tictactoe_inputs]
+#oldBoard = [[0,0,1./7,1./7],[0,0,1./7,2./7],[1./7,1./7,2./7,4./7],[3./7,5./7,7./7,5./7]]
+#next_move(oldBoard, 128, 1000, 3)
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
-        numberOfBoards = float(len(tictactoe_inputs))
-        genome.fitness = 1.0
-        net = neat.nn.FeedForwardNetwork.create(genome, config)
-
-        #fitnessBoard = [[0 for j in range(len(tictactoe_inputs))] for i in range(9)]
-
-        for xi, xo in zip(tictactoe_inputs, tictactoe_outputs):
-            output = net.activate(xi)
-
-            for i in range(9):
-                genome.fitness -= float((xo[i] - output[i]) ** 2) / (9.0 * numberOfBoards)
-
+        numGames = 10
+        for game in range(numGames):
+            genome.fitness = 0.0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            gameOver = False
+            currentBoard = [[0,0,0,0],[0,0.5,0,0],[0,0,1,0],[0,0,0,0]]
+            h = 4
+            while not gameOver:
+                moveMatrix = net.activate([currentBoard[i][j] for i in range(4) for j in range(4)])
+                gameOver = True # This halts the loop if the for loop returns False.
+                for i in sorted(range(4), key=lambda k: moveMatrix[k], reverse = True):
+                    oldScore = genome.fitness
+                    newBoard, newHighValue, newScore = next_move(currentBoard, h, oldScore, i)
+                    if False in [currentBoard[i][j]==newBoard[i][j] for i in range(4) for j in range(4)]:
+                        currentBoard = copy.deepcopy(newBoard)
+                        h = newHighValue
+                        genome.fitness = newScore
+                        gameOver = False
+                        #[print([int(2**(i*math.log(h,2))) if i!=0 else 0 for i in row]) for row in currentBoard]
+                        #print("")
+                        #time.sleep(1)
+                        break
+        genome.fitness = genome.fitness / numGames
 
 def run(config_file):
     # Load configuration.
@@ -140,7 +156,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(1000-1))
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 5000)
+    winner = p.run(eval_genomes, 10)
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
@@ -148,11 +164,32 @@ def run(config_file):
     # Show output of the most fit genome against training data.
     print('\nOutput:')
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-    for xi, xo in zip(tictactoe_inputs, tictactoe_outputs):
-        output = winner_net.activate(xi)
-        print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
+    oldScore = 0.0
+    gameOver = False
+    currentBoard = [[0,0,0,0],[0,2,0,0],[0,0,4,0],[0,0,0,0]]
+    h = 4
+    while not gameOver:
+        moveMatrix = winner_net.activate([currentBoard[i][j] for i in range(4) for j in range(4)])
+        gameOver = True
+        for i in sorted(range(4), key=lambda k: moveMatrix[k], reverse = True):
+            newBoard, newHighValue, newScore = next_move(currentBoard, h, oldScore, i)
+            if False in [currentBoard[i][j]==newBoard[i][j] for i in range(4) for j in range(4)]:
+                currentBoard = copy.deepcopy(newBoard)
+                h = newHighValue
+                oldScore = newScore
+                gameOver = False
+                break
+    print("Winning Score: {!r}, highest-valued tile {!r}".format(oldScore, h))
+    print("Final board state: ")
+    [print([2**(i*math.log(h,2)) if i!=0 else 0 for i in row]) for row in currentBoard]
 
-    node_names = {-1:'i 1,1', -2: 'i 1,2', -3: 'i 1,3', -4: 'i 2,1', -5: 'i 2,2', -6: 'i 2,3', -7: 'i 3,1', -8: 'i 3,2', -9: 'i 3,3', 0: 'o 1,1', 1: 'o 1,2', 2: 'o 1,3', 3: 'o 2,1', 4: 'o 2,2', 5: 'o 2,3', 6: 'o 3,1', 7: 'o 3,2', 8: 'o 3,3'}
+    node_names = {
+        -1: '1,1', -2: '1,2', -3: '1,3', -4: '1,4',
+        -5: '2,1', -6: '2,2', -7: '2,3', -8: '2,4',
+        -9: '3,1', -10: '3,2', -11: '3,3', -12: '3,4',
+        -13: '4,1', -14: '4,2', -15: '4,3', -16: '4,4',
+        0: 'Left', 1: 'Down', 2: 'Right', 3: 'Up'
+        }
     visualize.draw_net(config, winner, True, node_names=node_names)
     visualize.plot_stats(stats, ylog=False, view=True)
     #visualize.plot_species(stats, view=True)
@@ -166,5 +203,5 @@ if __name__ == '__main__':
     # here so that the script will run successfully regardless of the
     # current working directory.
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config-tictactoe')
+    config_path = os.path.join(local_dir, 'config-2048')
     run(config_path)
